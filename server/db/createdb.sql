9d0674e2fa76e17421548bee23ccda9ba660ddf0
Vedansh Agarwal <hithisisvedansh@gmail.com>
2022-06-30 16:50:36 +0530

    changes to the db

--- server/db/createdb.sql
+++ server/db/createdb.sql
@@ -24,56 +24,51 @@ CREATE TABLE `tempdb`.`user` (
   `last_modified_by` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`user_id`));
   
+/*.Creating user table */
+CREATE TABLE `tempdb`.`user_change_log` (
+  `change_id` INT NOT NULL AUTO_INCREMENT,
+  `change_by` VARCHAR(45) NOT NULL,
+  `change_type` VARCHAR(6) NOT NULL,
+  `change_timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `user_id` VARCHAR(45) NOT NULL,
+  `base_img` VARCHAR(45) NOT NULL,
+  `img_ext` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `mob_no` VARCHAR(45) NOT NULL,
+  `gender` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `department` VARCHAR(45) NOT NULL,
+  `date_created` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`change_id`));
+
 /* Creating admin logs table */
 CREATE TABLE `tempdb`.`admin_log` (
   `change_id` INT NOT NULL AUTO_INCREMENT,
   `change_by` VARCHAR(20) NOT NULL,
   `change_on` VARCHAR(36) NOT NULL,
   `change_type` VARCHAR(6) NOT NULL,
-  `change_values` VARCHAR(400) NOT NULL,
   `change_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`change_id`));
 
 /* Trigger for admin_log on insert user */
 DELIMITER $$
-CREATE DEFINER = CURRENT_USER TRIGGER `tempdb`.`admin_log_on_insert` AFTER INSERT ON `user` FOR EACH ROW
-BEGIN
-	SET @change_values = CONCAT("base_img = '", NEW.`base_img`, "'");
-	SET @change_values = CONCAT(@change_values, ", name='", NEW.`name`, "'");
-	SET @change_values = CONCAT(@change_values, ", mob_no='", NEW.`mob_no`, "'");
-	SET @change_values = CONCAT(@change_values, ", gender='", NEW.`gender`, "'");
-	SET @change_values = CONCAT(@change_values, ", city='", NEW.`city`, "'");
-	SET @change_values = CONCAT(@change_values, ", department='", NEW.`department`, "'");
-	INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`,`change_values`)
-    VALUE (NEW.`last_modified_by`, NEW.`user_id`, "INSERT", @change_values);
+CREATE DEFINER=`root`@`localhost` TRIGGER `admin_log_on_insert` AFTER INSERT ON `user` FOR EACH ROW BEGIN
+	INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`)
+    VALUE (NEW.`last_modified_by`, NEW.`user_id`, "INSERT");
+    INSERT INTO `user_change_log` (`change_by`, `change_type`, `user_id`, `base_img`, `img_ext`, `name`, `mob_no`, `gender`, `city`, `department`, `date_created`) 
+    VALUE (NEW.`last_modified_by`, "INSERT", NEW.`user_id`, NEW.`base_img`, NEW.`img_ext`, NEW.`name`, NEW.`mob_no`, NEW.`gender`, NEW.`city`, NEW.`department`, NEW.`date_created`);
 END$$
 DELIMITER ;
 
 /* Trigger for admin_log on update user */
 DELIMITER $$
-CREATE DEFINER = CURRENT_USER TRIGGER `tempdb`.`admin_log_on_update` AFTER UPDATE ON `user` FOR EACH ROW
-BEGIN
-	SET @change_values = "";
-    IF (NEW.`base_img` != OLD.`base_img`) THEN
-        SET @change_values = CONCAT(@change_values, "base_img='", NEW.`base_img`, "'");
+CREATE DEFINER=`root`@`localhost` TRIGGER `admin_log_on_update` AFTER UPDATE ON `user` FOR EACH ROW BEGIN
+    IF (NEW.`base_img` != OLD.`base_img` OR NEW.`name` != OLD.`name` OR NEW.`mob_no` != OLD.`mob_no` OR NEW.`gender` != OLD.`gender` OR NEW.`city` != OLD.`city` OR NEW.`department` != OLD.`department`) THEN
+        INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`)
+		VALUE (NEW.`last_modified_by`, NEW.`user_id`, "UPDATE");
+        INSERT INTO `user_change_log` (`change_by`, `change_type`, `user_id`, `base_img`, `img_ext`, `name`, `mob_no`, `gender`, `city`, `department`, `date_created`) 
+		VALUE (NEW.`last_modified_by`, "INSERT", NEW.`user_id`, NEW.`base_img`, NEW.`img_ext`, NEW.`name`, NEW.`mob_no`, NEW.`gender`, NEW.`city`, NEW.`department`, NEW.`date_created`);
     END IF;
-    IF (NEW.`name` != OLD.`name`) THEN
-        SET @change_values = CONCAT(@change_values, " name='", NEW.`name`, "'");
-    END IF;
-    IF (NEW.`mob_no` != OLD.`mob_no`) THEN
-        SET @change_values = CONCAT(@change_values, " mob_no='", NEW.`mob_no`, "'");
-    END IF;
-    IF (NEW.`gender` != OLD.`gender`) THEN
-        SET @change_values = CONCAT(@change_values, " gender='", NEW.`gender`, "'");
-    END IF;
-    IF (NEW.`city` != OLD.`city`) THEN
-        SET @change_values = CONCAT(@change_values, " city='", NEW.`city`, "'");
-    END IF;
-    IF (NEW.`department` != OLD.`department`) THEN
-        SET @change_values = CONCAT(@change_values, " department='", NEW.`department`, "'");
-    END IF;
-	INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`,`change_values`)
-    VALUE (NEW.`last_modified_by`, NEW.`user_id`, "UPDATE",@change_values);
 END$$
 DELIMITER ;
 
@@ -94,3 +89,12 @@ VIEW `tempdb`.`get_user` AS
         `tempdb`.`user`.`date_created` AS `date_created`
     FROM
         `tempdb`.`user`;
+        
+/* Increment capture_count procedure */
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `increment_capture_counter`(IN usr_id VARCHAR(36))
+BEGIN
+UPDATE `user` SET `captured_count` = `captured_count`+1 WHERE `user_id` = usr_id;
+SELECT `captured_count` FROM `user` WHERE `user_id` = usr_id;
+END$$
+DELIMITER ;
